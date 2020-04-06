0e0a7060b6cb586b10a08177d4993f6e0b2d9001
Oliver Schafeld <oliver@schafeld.com>
2020-04-06 18:28:11 +0200

    set up mocked articles

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the simple GO API!")
-    fmt.Println("Endpoint Hit: root directory")
+// Struct for articles
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+// Global array to mock article data
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the simple GO API!")
+	fmt.Println("Endpoint Hit: root directory")
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
-    fmt.Println("Started web server at http://localhost:8080")
-    handleRequests()
+	Articles = []Article{
+		Article{Title: "First Title", Desc: "Description of 1st article", Content: "Article One Content"},
+		Article{Title: "Second Title", Desc: "Descriptionof 2nd article", Content: "Article Two Content"},
+	}
+
+	fmt.Println("Starting web server at http://localhost:8080")
+	handleRequests()
 }
